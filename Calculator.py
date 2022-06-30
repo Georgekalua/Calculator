80c27f576f96f1512e03ec1d5b6044a02863cadd
Georgekalua <95909631+Georgekalua@users.noreply.github.com>
2022-06-30 19:00:30 +0200

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+#Accepting user input
+
+price=int(input("please enter price: "))
+items=int(input("please enter the number of items"))
+cost=str(price*items)
+
+print("you should pay  K"+cost)
+print("thank you for shoping with us.yOU WERE HELPED BY GEORGE KALUA")
